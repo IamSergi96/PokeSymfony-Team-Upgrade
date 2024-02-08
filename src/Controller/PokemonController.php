967ddd9947fd3c3d9a298f0159c33997f73b6056
Alba <albadvg@yahoo.es>
2024-02-08 17:54:36 +0100

    peticion a base de datos

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
 use ReturnTypeWillChange;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController
@@ -23,28 +26,43 @@ class PokemonController extends AbstractController
   }
 
   #[Route("/pokemons")]
-  public function listPokemons()
+  public function listPokemons(EntityManagerInterface $doctrine)
   {
-    $list = [
-      [
-        "nombre" => "Pikachu",
-        "description" => "amarillo",
-        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-        "codigo" => "25"
-      ],
-      [
-        "nombre" => "CHarizard",
-        "description" => "amarillo",
-        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
-        "codigo" => "6"
-      ], [
-        "nombre" => "Bulbasur",
-        "description" => "amarillo",
-        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-        "codigo" => "1"
-      ],
-    ];
+    $repositorio = $doctrine->getRepository(Pokemon::class);
+    $pokemons = $repositorio->findAll();
+    return $this->render("Pokemons/listPokemons.html.twig", ["pokemons" => $pokemons]);
+  }
 
-    return $this->render("Pokemons/listPokemons.html.twig", ["pokemons" => $list]);
+  #[Route("/insertPokemons")]
+  public function insertPokemons(EntityManagerInterface $doctrine)
+  {
+    $pokemon = new Pokemon();
+
+    $pokemon->setNombre('Charmander');
+    $pokemon->setDescription('Lorem ipsummmmsdfasdñfhañl');
+    $pokemon->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png');
+    $pokemon->setCodigo(4);
+
+    $pokemon2 = new Pokemon();
+
+    $pokemon2->setNombre('Pikachu');
+    $pokemon2->setDescription('Lorem ipsummmmsdfasdñfhañl');
+    $pokemon2->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png');
+    $pokemon2->setCodigo(6);
+
+    $pokemon3 = new Pokemon();
+
+    $pokemon3->setNombre('Charizar');
+    $pokemon3->setDescription('Lorem ipsummmmsdfasdñfhañl');
+    $pokemon3->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png');
+    $pokemon3->setCodigo(9);
+
+    $doctrine->persist($pokemon);
+    $doctrine->persist($pokemon2);
+    $doctrine->persist($pokemon3);
+
+    $doctrine->flush();
+
+    return new Response("Pokemons insertados correctamente");
   }
 }
