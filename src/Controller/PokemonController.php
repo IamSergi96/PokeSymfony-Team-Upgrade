6d065209170184400d804efbd6d39b7fc1b8a9ef
David <138037766+dhx101@users.noreply.github.com>
2024-02-08 15:10:28 +0100

    mostrar lista pokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -6,17 +6,45 @@ use ReturnTypeWillChange;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PokemonController extends AbstractController {
+class PokemonController extends AbstractController
+{
 
-    #[Route("/pokemon")]
+  #[Route("/pokemon")]
 
-    public function getPokemon(){
-      $pokemon=[
-        "nombre"=> "Pikachu",
-        "description"=> "amarillo",
-        "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-        "codigo"=>"1"
-      ];
- return $this->render("Pokemons/GetPokemon.html.twig",["pokemon"=>$pokemon]);   
-    }
+  public function getPokemon()
+  {
+    $pokemon = [
+      "nombre" => "Pikachu",
+      "description" => "amarillo",
+      "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+      "codigo" => "1"
+    ];
+    return $this->render("Pokemons/GetPokemon.html.twig", ["pokemon" => $pokemon]);
+  }
+
+  #[Route("/pokemons")]
+  public function listPokemons()
+  {
+    $list = [
+      [
+        "nombre" => "Pikachu",
+        "description" => "amarillo",
+        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+        "codigo" => "25"
+      ],
+      [
+        "nombre" => "CHarizard",
+        "description" => "amarillo",
+        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+        "codigo" => "6"
+      ], [
+        "nombre" => "Bulbasur",
+        "description" => "amarillo",
+        "img" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        "codigo" => "1"
+      ],
+    ];
+
+    return $this->render("Pokemons/listPokemons.html.twig", ["pokemons" => $list]);
+  }
 }
