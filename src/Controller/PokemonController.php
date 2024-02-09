2008bae447f9090c3951c37a65880cace9a01a58
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-02-09 16:36:01 +0100

    hemos añadido debilidades

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Debilidad;
 use App\Entity\Pokemon;
 use Doctrine\ORM\EntityManagerInterface;
 use ReturnTypeWillChange;
@@ -53,9 +54,33 @@ class PokemonController extends AbstractController
     $pokemon3->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png');
     $pokemon3->setCodigo(9);
 
+    //  Creamos las debilidades, y les damos las propiedades que queremos
+    $debilidad = new Debilidad();
+    $debilidad->setNombre('Fuego');
+
+    $debilidad2 = new Debilidad();
+    $debilidad2->setNombre('Tierra');
+
+    $debilidad3 = new Debilidad();
+    $debilidad3->setNombre('Agua');
+
+    $pokemon->addDebilidade($debilidad);
+    $pokemon->addDebilidade($debilidad3);
+
+    $pokemon2->addDebilidade($debilidad2);
+
+
+    // El persis es para que doctrine sepa que queremos instertar ese objeto en base de datos
+    // El flush es el que realmente hace la insercion o sea en plan lo registra
+
     $doctrine->persist($pokemon);
     $doctrine->persist($pokemon2);
     $doctrine->persist($pokemon3);
+    $doctrine->persist($debilidad);
+    $doctrine->persist($debilidad2);
+    $doctrine->persist($debilidad3);
+
+
 
     $doctrine->flush();
 
